e8d66ef0571a9ade0c3f260c2bd424d175fbea0a
914JugariuDiana <dia.jugariu@gmail.com>
2023-04-25 17:17:37 +0300

    part1

---
+++ react-frontend/src/components/ingredients/IngredientDetails.tsx
@@ -0,0 +1,58 @@
+import { Card, CardActions, CardContent, IconButton } from "@mui/material";
+import { Container } from "@mui/system";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { BACKEND_API_URL } from "../../constants";
+import {Ingredient} from "../../models/Ingredient";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+
+export const IngredientDetails = () => {
+	const { id } = useParams();
+	const [ingredient, setCourse] = useState<Ingredient>();
+
+	useEffect(() => {
+		const fetchCourse = async () => {
+			// TODO: use axios instead of fetch
+			// TODO: handle errors
+			// TODO: handle loading state
+			const response = await fetch(`${BACKEND_API_URL}/ingredients/${id}/`);
+			const ingredient = await response.json();
+			setCourse(ingredient);
+		};
+		fetchCourse();
+	}, [id]);
+
+	return (
+		<Container>
+			<Card>
+				<CardContent>
+					<IconButton component={Link} sx={{ mr: 3 }} to={`/ingredients/`}>
+						<ArrowBackIcon />
+					</IconButton>{" "}
+					<h1>Ingredient Details</h1>
+					<p>Ingredient name: {ingredient?.ingredientName}</p>
+					<p>Ingredient quantity: {ingredient?.quantity}</p>
+					<p>Running low: {ingredient?.runningLow ? "yes" : "no" }</p>
+                    <p>Expiration date: {ingredient?.expirationDate}</p>
+					<p>Ingredient Foods:</p>
+					<ul>
+						{ingredient?.foods?.map((food) => (
+							<li key={food.foodId}>{food.foodId}</li>
+						))}
+					</ul>
+				</CardContent>
+				<CardActions>
+					<IconButton component={Link} sx={{ mr: 3 }} to={`/ingredients/${id}/edit`}>
+						<EditIcon />
+					</IconButton>
+
+					<IconButton component={Link} sx={{ mr: 3 }} to={`/ingredients/${id}/delete`}>
+						<DeleteForeverIcon sx={{ color: "red" }} />
+					</IconButton>
+				</CardActions>
+			</Card>
+		</Container>
+	);
+};
