6bcc0e1861c5f1c4261123de9a90d7e10de920fa
914JugariuDiana <dia.jugariu@gmail.com>
2023-05-06 12:54:26 +0300

    6.4

--- react-frontend/src/constants.ts
+++ react-frontend/src/constants.ts
@@ -1,3 +1,3 @@
-const DEV_BACKEND_API_URL = "http://ec2-52-207-182-23.compute-1.amazonaws.com:80/api"
+const DEV_BACKEND_API_URL = "http://ec2-54-210-117-204.compute-1.amazonaws.com:80/api"
 
 export const BACKEND_API_URL = DEV_BACKEND_API_URL;
